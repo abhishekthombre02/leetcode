20122fcd9a2f49dcc67a696eda252bb9b36c3f7a
Abhishek Thombre <137371709+abhishekthombre02@users.noreply.github.com>
2024-06-03 18:46:37 +0530

    Time: 17 ms (98.41%), Space: 57.1 MB (55.83%) - LeetHub

--- 1798-max-number-of-k-sum-pairs/1798-max-number-of-k-sum-pairs.java
+++ 1798-max-number-of-k-sum-pairs/1798-max-number-of-k-sum-pairs.java
@@ -3,14 +3,14 @@ class Solution {
 
         Arrays.sort(nums);
         int left=0,right=nums.length-1,count=0;
-
         while(left<right){
-            if(nums[left]+nums[right]==k){
+            int sum=nums[left]+nums[right];
+            if(sum==k){
                 count++;
                 left++;
                 right--;
             }
-            else if(nums[left]+nums[right]<k){
+            else if(sum<k){
                 left++;
             }
             else{
